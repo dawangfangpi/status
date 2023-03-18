6c0bd9d53d0440bf442a0f9573e00facb28dba6e
jdnjk <2394058778@qq.com>
2023-03-18 11:01:48 +0800

    上次

---
+++ config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Monday状态检测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1850439-d9cb0683fd87967259e49ad1',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '官网',
+      url: 'https://www.monday-ovo.top/'
+    },
+    {
+      text: '论坛',
+      url: 'https://bbs.monday-ovo.eu.org'
+    },
+  ],
+};
